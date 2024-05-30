d0622165f51583cea0218a2372ab1fc1dc6a3bff
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-30 22:30:47 +0300

    app.routes updated for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
+import { CampaignListComponent } from './pages/campaign-list/campaign-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "login", component: LoginComponent },
+    { path: "register", component: RegisterComponent },
+    { path: "campaign-list", component: CampaignListComponent },
+];
