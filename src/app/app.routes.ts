2f3bdbd304b09c774685756c6d990d8166099d63
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-05-31 15:35:47 +0300

    added authGuard excluded login and register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,22 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from "./pages/register/register.component";
 import { CampaignListComponent } from './pages/campaign-list/campaign-list.component';
+import { authGuard } from './guards/auth-guard.guard';
 
 export const routes: Routes = [
     { path: "login", component: LoginComponent },
     { path: "register", component: RegisterComponent },
-    { path: "campaign-list", component: CampaignListComponent },
+    {
+        path: "",
+        redirectTo: "campaign-list",
+        pathMatch: "full"
+    },
+    {
+        path: "",
+        canActivateChild: [authGuard],
+        children: [
+            { path: "campaign-list", component: CampaignListComponent },
+            // { path: "campaign-create", component: CampaignListComponent }
+        ]
+    }
 ];
