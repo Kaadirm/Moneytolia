088ba2033e5a43b65de2c777e701b98082cc67d7
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-06-03 17:49:23 +0300

    header component functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent,
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  username: string | null = null;
 
+  constructor(private AuthService: AuthService) { }
+
+  ngOnInit() {
+    // Call a method in your AuthService to get the username
+    this.username = this.AuthService.getUsername();
+  }
 }
