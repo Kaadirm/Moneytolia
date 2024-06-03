946b6541f3d1ec5a8c2dc4cf1b168520e1145d99
Kaadirm <141996672+Kaadirm@users.noreply.github.com>
2024-06-03 17:51:16 +0300

    atuh guard updates

--- src/app/guards/auth-guard.guard.ts
+++ src/app/guards/auth-guard.guard.ts
@@ -1,6 +1,7 @@
-import { inject } from "@angular/core";
+import { inject, PLATFORM_ID } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateChildFn, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "../services/auth.service";
+import { isPlatformBrowser } from "@angular/common";
 
 export const authGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
@@ -8,12 +9,30 @@ export const authGuard: CanActivateFn = (
 ) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  // const platformId = inject(PLATFORM_ID);
 
+  // if (isPlatformBrowser(platformId)) {
+
+  // }
   const isAuthenticated = authService.isAuthenticated()
   if (!isAuthenticated) {
     router.navigate(["/login"]);
     return false;
   }
+  // afterRender(() => {
+  //   const isAuthenticated = authService.isAuthenticated();
+  //   if (!isAuthenticated) {
+  //     router.navigate(["/login"]);
+  //     return false;
+  //   }
+  //   return true;
+  // }, { phase: AfterRenderPhase.EarlyRead });
+
+  // const isAuthenticated = authService.isAuthenticated("/");
+  // if (!isAuthenticated) {
+  //   router.navigate(["/login"]);
+  //   return false;
+  // }
 
   return true;
 };
